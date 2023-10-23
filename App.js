ad443b253426fae542a869fd308fa676475d738f
Rannie Reyes <reyes.rannied@gmail.com>
2023-10-23 09:06:05 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import "./App.scss";
+import Login from "./screen/login/Login";
+
+function App() {
+  return <Login />;
+}
+
+export default App;
---
+++ screen/login/Login.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+const Login = () => {
+  return <Box>1</Box>;
+};
+
+export default Login;
